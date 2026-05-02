e3f9376daef888fe4ad50a513318137ead35d395
littlefly365 <c081526@hotmail.com>
2026-05-02 16:46:39 -0500

    add PROGNAME/OPTS macro and option parser on host/domainname

--- Source/domainname/domainname.c
+++ Source/domainname/domainname.c
@@ -31,11 +31,41 @@
 
 #include "utils.h"
 
+#define PROGNAME "domainname"
+#define OPTS "hV"
+
+static char USAGE[] =
+	"Usage: " PROGNAME " [NAME]\n"
+	"or: " PROGNAME " [OPTION]\n"
+	"Show or set the system's domain name.\n\n"
+	"\t-h\t\tshow this help and exit\n"
+	"\t-V\t\tshow version information and exit";
+	
 int
 main(int argc, char *argv[])
 {
+	int c;
 	char domainname[MAXHOSTNAMELEN];
-	++argv;
+	while ((c = parse_options(OPTS)) != -1) {
+		switch (c) {
+			case 'h':
+				puts(USAGE);
+				return EXIT_SUCCESS;
+				break;
+			case 'V':
+				printf("%s %s\n", PROGNAME, PROG_VERSION);
+				return EXIT_SUCCESS;
+				break;
+			default:
+				fprintf(stderr, "Try '%s -h' for more information\n", PROGNAME);
+				return EXIT_SUCCESS;
+				break;
+			}
+		}
+
+	argc -= optind;
+	argv += optind;
+
 	if (*argv) { 
 		if (setdomainname(*argv, strlen(*argv)))
 			err(1, "setdomainname");
@@ -44,7 +74,6 @@ main(int argc, char *argv[])
 		if (getdomainname(domainname, sizeof(domainname)))
 			err(1, "getdomainname");
 		puts(domainname);
-		return EXIT_SUCCESS;
 	}
 
 	return EXIT_SUCCESS;
